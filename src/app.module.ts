b749258df0ca31958717744ca6af687292d4ed81
tfkhdyt <tfkhdyt@pm.me>
2022-04-02 15:00:53 +0700

    delete module playing

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CitiesModule } from './cities/cities.module'
-import { PlayingModule } from './playing/playing.module';
 
 @Module({
-  imports: [CitiesModule, PlayingModule],
+  imports: [CitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/playing/playing.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PlayingController } from './playing.controller';
-import { PlayingService } from './playing.service';
-
-@Module({
-  controllers: [PlayingController],
-  providers: [PlayingService]
-})
-export class PlayingModule {}
