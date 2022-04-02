83b0227418007ddff3425609f735da782dbc1ca4
tfkhdyt <tfkhdyt@pm.me>
2022-04-02 12:07:09 +0700

    add cities module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [],
+  imports: [CitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
