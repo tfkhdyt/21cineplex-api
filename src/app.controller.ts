ac198fb313e9483a8b62972c7423032b40f14d8e
tfkhdyt <tfkhdyt@pm.me>
2022-04-05 05:30:11 +0700

    exclude app controller from swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Redirect } from '@nestjs/common'
+import { ApiExcludeController } from '@nestjs/swagger'
 
+@ApiExcludeController()
 @Controller()
 export class AppController {
   @Get()
